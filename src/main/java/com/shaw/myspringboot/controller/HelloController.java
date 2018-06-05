b4b029fe19d9158569a58f7b862fc876933e97a8
NicoX <nicoxu1994@outlook.com>
2018-06-05 23:02:19 +0800

    配置静态资源html

--- src/main/java/com/shaw/myspringboot/controller/HelloController.java
+++ src/main/java/com/shaw/myspringboot/controller/HelloController.java
@@ -3,25 +3,33 @@ package com.shaw.myspringboot.controller;
 import com.shaw.myspringboot.entity.TblUser;
 import com.shaw.myspringboot.service.TblUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     private TblUserService tblUserService;
 
+    @ResponseBody
     @RequestMapping(value = "/helloWorld",method = RequestMethod.GET)
     public List<TblUser> hello() {
         return tblUserService.getUserList();
     }
 
+    @ResponseBody
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String world() {
         return "hello world";
+    }
+
+    @RequestMapping(value = "toIndex",method = RequestMethod.GET)
+    public String toIndex() {
+        return "index";
     }
 }
